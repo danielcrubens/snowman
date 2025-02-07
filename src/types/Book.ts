08c7374a55b32f4ed766e671a52550ec30d50eb0
danielcrubens <dcr.tecnologi@gmail.com>
2025-02-07 15:12:33 -0300

    feat: implementando tipagem

---
+++ src/types/Book.ts
@@ -0,0 +1,9 @@
+export interface Book {
+  id: string
+  title: string
+  author: string
+  description: string
+  coverUrl: string
+  available: boolean
+  price: number
+}
---
+++ src/types/Rental.ts
@@ -0,0 +1,7 @@
+export interface Rental {
+  id: string
+  bookId: string
+  userId: string
+  rentalDate: Date
+  returnDate: Date
+}
