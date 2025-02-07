a5dca73ac78f0da2ca8f55739156c96b313b6ede
danielcrubens <dcr.tecnologi@gmail.com>
2025-02-07 15:53:49 -0300

    feat: implementando inicialização comp pinia i18n

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import '/src/assets/css/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router'
+import { createI18n } from 'vue-i18n'
 
-const pinia = createPinia()
 const app = createApp(App)
+const pinia = createPinia()
+const i18n = createI18n({
+  locale: 'pt',
+  fallbackLocale: 'en',
+  messages: {
+    pt: await import('@/locales/pt.json'),
+    en: await import('@/locales/en.json')
+  }
+})
 
 app.use(pinia)
+app.use(router)
+app.use(i18n)
 app.mount('#app')
